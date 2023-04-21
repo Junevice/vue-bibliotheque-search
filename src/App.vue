5f0ec19c617061109f4da3bbefa29354cc00ceff
Junevice <june.webfreelance@gmail.com>
2023-04-21 10:10:26 +0200

    Remove useless files

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <p class="text-2xl">Hey, I'm the navbar</p>
   </header>
 
   <RouterView />
